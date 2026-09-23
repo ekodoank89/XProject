52b9e267ccadcaa80f68d9e4fece9e568980f4bd
ekodoank89 <ayaekodoank@gmail.com>
2026-09-23 22:58:45 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/aya/xproject/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.aya.xproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.aya.xproject.ui.maps.MapsScreen
+import com.aya.xproject.ui.theme.XProjectTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            XProjectTheme {
+                MapsScreen()
+            }
+        }
+    }
+}
