aee0e86709229b3082ecd0a9c90fc23ea95aecff
ekodoank89 <ayaekodoank@gmail.com>
2026-09-23 23:39:51 +0700

    Update MainActivity.kt

--- app/src/main/java/com/aya/xproject/MainActivity.kt
+++ app/src/main/java/com/aya/xproject/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.aya.xproject.ui.maps.MapsScreen
+import com.aya.xproject.ui.main.MainScreen
 import com.aya.xproject.ui.theme.XProjectTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             XProjectTheme {
-                MapsScreen()
+                MainScreen()
             }
         }
     }
