948e6f4ecd078f4984ee8229cd37ee28a271d706
ekodoank89 <ayaekodoank@gmail.com>
2026-09-23 23:47:15 +0700

    Update MainScreen.kt

--- app/src/main/java/com/aya/xproject/ui/main/MainScreen.kt
+++ app/src/main/java/com/aya/xproject/ui/main/MainScreen.kt
@@ -1,12 +1,12 @@
 package com.aya.xproject.ui.main
 
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.dp
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
