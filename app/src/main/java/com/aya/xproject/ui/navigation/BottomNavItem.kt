c109ee912b6b31b30709a76e733e84ebe1097723
ekodoank89 <ayaekodoank@gmail.com>
2026-09-23 23:36:01 +0700

    Create BottomNavItem.kt

---
+++ app/src/main/java/com/aya/xproject/ui/navigation/BottomNavItem.kt
@@ -0,0 +1,25 @@
+package com.aya.xproject.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomNavItem(
+    val route: String,
+    val label: String,
+    val icon: ImageVector
+) {
+    data object Favorite : BottomNavItem("favorite", "FAV", Icons.Filled.Favorite)
+    data object Jitter : BottomNavItem("jitter", "JIT", Icons.Filled.Refresh)
+    data object Home : BottomNavItem("home", "HOME", Icons.Filled.Home)
+    data object Option : BottomNavItem("option", "OPT", Icons.Filled.MoreVert)
+    data object Setting : BottomNavItem("setting", "SET", Icons.Filled.Settings)
+
+    companion object {
+        val items = listOf(Favorite, Jitter, Home, Option, Setting)
+    }
+}
