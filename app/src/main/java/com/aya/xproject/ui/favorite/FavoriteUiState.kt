ca9879609de2bb30c85445cf459618ddbb3238b9
ekodoank89 <ayaekodoank@gmail.com>
2026-09-24 14:42:07 +0700

    Create FavoriteUiState.kt

---
+++ app/src/main/java/com/aya/xproject/ui/favorite/FavoriteUiState.kt
@@ -0,0 +1,21 @@
+package com.aya.xproject.ui.favorite
+
+import com.aya.xproject.domain.model.FavoriteTab
+
+data class FavoriteUiState(
+    val selectedTab: FavoriteTab = FavoriteTab.GRB,
+    val form: FavoriteFormState = FavoriteFormState()
+)
+
+data class FavoriteFormState(
+    val isFromPin: Boolean = true,   // true = DARI PIN, false = MANUAL
+    val editingId: Long? = null,     // non-null saat mode edit
+    val name: String = "",
+    val latitude: String = "",
+    val longitude: String = "",
+    val nameError: String? = null,
+    val latitudeError: String? = null,
+    val longitudeError: String? = null
+) {
+    val isEditing: Boolean get() = editingId != null
+}
