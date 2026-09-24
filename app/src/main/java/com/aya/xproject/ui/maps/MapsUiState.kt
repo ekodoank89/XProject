3adaa0b7f417626d9d5f72650f791a36d730dcde
ekodoank89 <ayaekodoank@gmail.com>
2026-09-24 22:58:01 +0700

    Update MapsUiState.kt

--- app/src/main/java/com/aya/xproject/ui/maps/MapsUiState.kt
+++ app/src/main/java/com/aya/xproject/ui/maps/MapsUiState.kt
@@ -2,10 +2,10 @@ package com.aya.xproject.ui.maps
 
 import com.aya.xproject.domain.model.MapCenter
 
-// Ubah koordinat default (Jakarta) sesuai kebutuhan
 data class MapsUiState(
     val center: MapCenter = MapCenter(-6.2088, 106.8456),
     val zoom: Float = 15f,
+    val isCenterLoaded: Boolean = false,
     val isCoordinateChipVisible: Boolean = true,
     val isGrbChipVisible: Boolean = true,
     val isGjkChipVisible: Boolean = true,
