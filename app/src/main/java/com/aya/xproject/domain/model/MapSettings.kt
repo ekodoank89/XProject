6e861ac6e48c0fd8cc5bc29fabb7ed2db0cc6396
ekodoank89 <ayaekodoank@gmail.com>
2026-09-24 18:59:41 +0700

    Update MapSettings.kt

--- app/src/main/java/com/aya/xproject/domain/model/MapSettings.kt
+++ app/src/main/java/com/aya/xproject/domain/model/MapSettings.kt
@@ -5,5 +5,7 @@ package com.aya.xproject.domain.model
  * Saat ada opsi baru, cukup tambahkan properti di sini.
  */
 data class MapSettings(
-    val isCoordinateChipVisible: Boolean = true
+    val isCoordinateChipVisible: Boolean = true,
+    val isGrbChipVisible: Boolean = true,
+    val isGjkChipVisible: Boolean = true
 )
