0ab03e9b970480a75e7879e7c2c86269cfe74308
ekodoank89 <ayaekodoank@gmail.com>
2026-09-24 16:02:32 +0700

    Update MainActivity.kt

--- app/src/main/java/com/aya/xproject/MainActivity.kt
+++ app/src/main/java/com/aya/xproject/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import com.aya.xproject.ui.main.MainScreen
+import com.aya.xproject.ui.splash.SplashScreen
 import com.aya.xproject.ui.theme.XProjectTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +20,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             XProjectTheme {
-                MainScreen()
+                var showSplash by rememberSaveable { mutableStateOf(true) }
+                if (showSplash) {
+                    SplashScreen(onFinished = { showSplash = false })
+                } else {
+                    MainScreen()
+                }
             }
         }
     }
