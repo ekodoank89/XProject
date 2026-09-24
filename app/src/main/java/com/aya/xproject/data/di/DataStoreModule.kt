0e842f289f8de6d5b1743427b028074249fa5e6f
ekodoank89 <ayaekodoank@gmail.com>
2026-09-24 22:50:37 +0700

    Create DataStoreModule.kt

---
+++ app/src/main/java/com/aya/xproject/data/di/DataStoreModule.kt
@@ -0,0 +1,26 @@
+package com.aya.xproject.data.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private val Context.xProjectDataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "xproject_prefs"
+)
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
+        context.xProjectDataStore
+}
