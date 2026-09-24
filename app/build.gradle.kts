88cf1f5095674a55a98b31e04a13832b90a60a8e
ekodoank89 <ayaekodoank@gmail.com>
2026-09-24 16:53:25 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
 
     implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
     implementation(libs.maps.compose)
 
     implementation(libs.androidx.room.runtime)
