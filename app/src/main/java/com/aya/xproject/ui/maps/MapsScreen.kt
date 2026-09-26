7bdc8ec9089d38e57977d81920551f29e663e88d
ekodoank89 <ayaekodoank@gmail.com>
2026-09-26 21:26:55 +0700

    Update MapsScreen.kt

--- app/src/main/java/com/aya/xproject/ui/maps/MapsScreen.kt
+++ app/src/main/java/com/aya/xproject/ui/maps/MapsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -37,7 +38,6 @@ fun MapsScreen(
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            // Konten Peta / Map UI
             Text(text = "Maps Screen Loaded")
         }
     }
