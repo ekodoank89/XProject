18e98e69f0b871c2fe7a207cb48b831e1103cd70
ekodoank89 <ayaekodoank@gmail.com>
2026-09-26 21:50:14 +0700

    Update MainActivity.kt

--- app/src/main/java/com/aya/xproject/MainActivity.kt
+++ app/src/main/java/com/aya/xproject/MainActivity.kt
@@ -3,28 +3,23 @@ package com.aya.xproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import com.aya.xproject.ui.main.MainScreen
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import com.aya.xproject.ui.splash.SplashScreen
-import com.aya.xproject.ui.theme.XProjectTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            XProjectTheme {
-                var showSplash by rememberSaveable { mutableStateOf(true) }
-                if (showSplash) {
-                    SplashScreen(onFinished = { showSplash = false })
-                } else {
-                    MainScreen()
+            MaterialTheme {
+                Surface {
+                    SplashScreen(
+                        onNavigateNext = {
+                            // Navigasi ke tampilan utama
+                        }
+                    )
                 }
             }
         }
